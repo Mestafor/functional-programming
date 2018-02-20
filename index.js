39f8eb3d618b8ee753ab61a2dac643c74a497221
Alex Iskander <Alex@>
2018-02-20 17:05:03 +0200

    pipe and compose

--- index.js
+++ index.js
@@ -3,9 +3,11 @@
 const partial = require('./src/functions/partial');
 const swap = require('./src/functions/swap');
 const filter = require('./src/lists/filter');
+const some = require('./src/lists/some');
+const sort = require('./src/lists/sort');
 const curry = require('./src/functions/curry');
 
-swap((a,b) => console.log(a, b))(1,2);
+swap((a, b) => console.log(a, b))(1, 2);
 
 /**
  * Curry
@@ -18,7 +20,8 @@ log('message');
 // checker
 const is = curry((fn, value) => fn(value));
 const isNumber = is((value) => typeof value === 'number');
-let numbers = ['1', 2, '3', 4];
+const isArray = is(value => value instanceof Array);
+let numbers = ['1', 2, '3', 4, 2, 6, 'asds'];
 let r = curry(swap(filter))(isNumber)(numbers);
 console.log(r);
 
@@ -29,23 +32,34 @@ const createTimer = partial((fn, ms) => setTimeout(fn, ms), undefined, 500);
 createTimer(() => console.log('Showed after 500ms'));
 
 
- /**
-  * Composition
-  */
- function compose(...fns) {
+/**
+ * Composition
+ */
+function compose(...fns) {
     return (args) => {
         let result = args;
-        for(let fn of fns.reverse()) {
+        for (let fn of [...fns.reverse()]) {
             result = fn(result);
-            console.log(result);
         }
 
         return result;
     }
- }
+}
 
- let filterNumbersAndLog = partial(
+function pipe(...fns) {
+    return (args) => {
+        let result = args;
+        for (let fn of fns) {
+            result = fn(result);
+        }
+
+        return result;
+    }
+}
+
+let filterNumbersAndLog = compose(
+    partial(sort, undefined, (a, b) => a > b),
     partial(filter, undefined, isNumber)
- );
+);
 
- filterNumbersAndLog(numbers);
+console.log(filterNumbersAndLog(numbers));
