11ff798a5b9f18b6eccb4c748e85229a75ad8ed9
Mestafor <Alex@>
2018-04-05 20:35:24 +0000

    testing Either

--- src/Monad/Either/index.js
+++ src/Monad/Either/index.js
@@ -1,29 +1,37 @@
 // const Either = Left || Right;
 
-const Right = x =>  ({
+const Right = x => ({
   map: f => Right(f(x)),
   fold: (f, g) => g(x),
   inspect: () => `Right(${x})`
 });
 
-const Left = x =>  ({
+const Left = x => ({
   map: f => Left(x),
   fold: (f, g) => f(x),
   inspect: () => `Left(${x})`
 });
 
-const findColor = name => {
-  const color = ({
-    green: 'green',
-    blue: 'blue',
-    red: 'red'
-  })[name]
+const Either = x => x != null ? Right(x) : Left(null);
 
-  return color ? Right(color) : Left(color);
+const tryCatch = f => {
+  try {
+    return Right(f());
+  } catch(e) {
+    return Left(e);
+  }
 }
 
-const res = Right(3).map(x => x*2).fold( () => 'error', x => x*10);
-const res2 = Left(3).map(x => x*2).fold(() => 'error', x => x*10);
-const color = findColor('green').map(x => x.toUpperCase()).fold(() => 'no color', x => x);
+const findColor = name => Either(({
+  green: 'green',
+  blue: 'blue',
+  red: 'red'
+})[name]);
 
-console.log(res, res2, color);
+const res = Right(3).map(x => x * 2).fold(() => 'error', x => x * 10);
+const res2 = Left(3).map(x => x * 2).fold(() => 'error', x => x * 10);
+const color = findColor('red').map(x => x.toUpperCase()).fold(x => x, x => x);
+
+const getPort = tryCatch(() => JSON.parse('{port:3300}')).map(item => item.port).fold(e => 3000, x => x);
+
+console.log(res, res2, color, getPort);
