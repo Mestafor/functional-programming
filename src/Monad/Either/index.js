a4ca38182c6d4881c9fee6d3b6a454819cef2a10
Mestafor <Alex@>
2018-04-06 20:36:21 +0000

    test Semigroup

--- src/Monad/Either/index.js
+++ src/Monad/Either/index.js
@@ -1,14 +1,18 @@
 // const Either = Left || Right;
 
 const Right = x => ({
+  chain: f => f(x),
   map: f => Right(f(x)),
   fold: (f, g) => g(x),
+  concat: o => o.fold(e => Left(e), r =>  Right(x.concat(r)) ),
   inspect: () => `Right(${x})`
 });
 
 const Left = x => ({
+  chain: f => Left(x),
   map: f => Left(x),
   fold: (f, g) => f(x),
+  concat: o => Left(x),
   inspect: () => `Left(${x})`
 });
 
@@ -32,6 +36,8 @@ const res = Right(3).map(x => x * 2).fold(() => 'error', x => x * 10);
 const res2 = Left(3).map(x => x * 2).fold(() => 'error', x => x * 10);
 const color = findColor('red').map(x => x.toUpperCase()).fold(x => x, x => x);
 
-const getPort = tryCatch(() => JSON.parse('{port:3300}')).map(item => item.port).fold(e => 3000, x => x);
+const getPort = tryCatch(() => JSON.parse('{"port":3300}')).chain(item => tryCatch(() => JSON.parse('{port:4000}'))).map(item => item.port).fold(e => 3000, x => x);
 
 console.log(res, res2, color, getPort);
+
+Either(undefined).chain(item => tryCatch(() => JSON.parse('{"port":3300}').port * item)).map(item => item / 3).fold(e => console.log('null we have ', e), item => console.log(item));
